64bf5c093e20989e2936690bc7af6934903b9f4d
Aaron Gil Rubinos <142986554+AaronRubinos@users.noreply.github.com>
2024-04-09 00:41:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+   <head>
+        <title>ITLEL2-18</title>
+   </head>
+   <body>
+   <center>
+   <hr/>
+   <h1>BootStrap</h1>
+   <h3>Section: 3BSCS-1</h3>
+   <hr/>
+   <img src="Aaron.jpg" width="200" height="200"/>
+   <p>Name: Aaron Gil B. Rubinos</p>
+   <h3>BOOTSTRAP ACTIVITIES</h3>
+   
+         <a href="Activity1">Activity1</a>
+		 <br/>
+		 <a href="Activity2">Activity2</a>
+		 <br/>
+		 <a href="Activity3">Activity3</a>
+         <br/>
+		 <a href="Activity4">Activity4</a>
+         <br/>
+		 <a href="Activity5">Activity5</a>
+         <br/>
+		 <a href="Activity6">Activity6</a>
+         <br/>
+		 <a href="Portfolio">Portfolio</a>
+		 <br/>
+		 <a href="photo_gallery">Photo Gallery</a>
+		 <br/>
+	     <a href="blog_project">Blog</a>
+		 <br/>
+	     <a href="e_commerce">E-Commerce</a>
+   </body>
+   </center>
+
+</html>
